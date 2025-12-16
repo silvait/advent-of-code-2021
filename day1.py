2e06b034caa04474490503b1bea70ab535977528
Paulo Silva <paulo@silvait.com>
2025-12-16 18:03:09 -0500

    add mode and encoding to open

--- day1.py
+++ day1.py
@@ -29,7 +29,7 @@ def part2(numbers: list[int]) -> int:
 if __name__ == "__main__":
     filename = "./sample.txt" if len(sys.argv) < 2 else sys.argv[1]
 
-    with open(filename) as f:
+    with open(filename, "r", encoding="utf-8") as f:
         parsed_data = parse_input(f.read())
         print(part1(parsed_data))
         print(part2(parsed_data))
