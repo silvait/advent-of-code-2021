2821e0e15ae80a1876fee70e80848a3f2a69604c
Paulo Silva <paulo@silvait.com>
2025-12-16 14:37:42 -0500

    add a template file for solutions

---
+++ template.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def parse_input(data):
+    return []
+
+
+def part1(*args):
+    total = 0
+    return total
+
+
+def part2(*args):
+    total = 0
+    return total
+
+
+if __name__ == "__main__":
+    filename = "./sample.txt" if len(sys.argv) < 2 else sys.argv[1]
+
+    with open(filename) as f:
+        parsed_data = parse_input(f.read())
+        print(part1(parsed_data))
+        print(part2(parsed_data))
