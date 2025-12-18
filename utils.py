ab24823a4ab8be3e9674cb9b8e52b2a6aee3cd76
Paulo Silva <paulo@silvait.com>
2025-12-18 00:16:27 -0500

    Add points function and compare

--- utils.py
+++ utils.py
@@ -3,3 +3,30 @@ def get_input_filename(argv):
         return argv[0].replace(".py", ".txt")
 
     return argv[1]
+
+
+def compare(a, b):
+    if a < b:
+        return -1
+
+    if a > b:
+        return 1
+
+    return 0
+
+
+def get_line_points(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+
+    step_x = compare(x1, x2)
+    step_y = compare(y1, y2)
+
+    points = [p1]
+
+    while (x1, y1) != p2:
+        x1 -= step_x
+        y1 -= step_y
+        points.append((x1, y1))
+
+    return points
