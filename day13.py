0b2ae252c09d9ce0ff221a70ab26463edd2f2081
Paulo Silva <paulo@silvait.com>
2025-12-24 01:03:25 -0500

    refactor

--- day13.py
+++ day13.py
@@ -12,9 +12,7 @@ def parse_input(data: str) -> tuple[list[tuple], dict[tuple[int, int], str]]:
         if line.startswith("fold"):
             [(axis, n)] = re.findall(r"fold along (.)=(\d+)", line)
             folds.append((axis, int(n)))
-        elif not line:
-            continue
-        else:
+        elif line:
             x, y = map(int, line.split(","))
             grid[(x, y)] = "#"
 
@@ -23,20 +21,15 @@ def parse_input(data: str) -> tuple[list[tuple], dict[tuple[int, int], str]]:
 
 def fold_grid(grid, axis, number):
     new_grid = {}
+    axis = 1 if axis == "y" else 0
 
-    for (x, y), v in grid.items():
-        if axis == "y":
-            if y < number:
-                new_grid[(x, y)] = v
-            else:
-                new_y = y - (2 * (y - number))
-                new_grid[(x, new_y)] = v
-        else:
-            if x < number:
-                new_grid[(x, y)] = v
-            else:
-                new_x = x - (2 * (x - number))
-                new_grid[(new_x, y)] = v
+    for coord, v in grid.items():
+        new_coord = list(coord)
+
+        if coord[axis] > number:
+            new_coord[axis] = (2 * number) - coord[axis]
+
+        new_grid[tuple(new_coord)] = v
 
     return new_grid
 
@@ -49,14 +42,12 @@ def part1(folds, grid) -> int:
     axis, number = folds[0]
     new_grid = fold_grid(grid, axis, number)
 
-    total = count_dots(new_grid)
-    return total
+    return count_dots(new_grid)
 
 
 def print_grid(grid):
     max_x, max_y = get_grid_size(grid)
 
-    print(max_x, max_y)
     for y in range(max_y + 1):
         for x in range(max_x + 1):
             print(grid.get((x, y), "."), end="")
@@ -64,25 +55,19 @@ def print_grid(grid):
 
 
 def get_grid_size(grid):
-    max_x = 0
-    max_y = 0
+    size = (0, 0)
 
     for x, y in grid.keys():
-        if x > max_x:
-            max_x = x
+        size = (max(size[0], x), max(size[1], y))
 
-        if y > max_y:
-            max_y = y
-
-    return (max_x, max_y)
+    return size
 
 
-def part2(folds, grid) -> int:
+def part2(folds, grid):
     for axis, number in folds:
         grid = fold_grid(grid, axis, number)
 
     print_grid(grid)
-    return 0
 
 
 if __name__ == "__main__":
